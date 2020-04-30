8376fb6ea33bd5b4db1fbb23e87b8a9d9a526696
Pedro Gonzalez <pgonzalez@post.com>
2020-04-29 21:37:08 -0500

    Configuramos dotenv

--- database/index.js
+++ database/index.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-const MONGO_URI = 'mongodb+srv://pedro:abc123def@cluster0-bbobj.mongodb.net/test?retryWrites=true&w=majority';
-
-mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(res => console.log('Conectado a la BD correctamente', res))
   .catch(err => console.log('Error al conectarme...', err));
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 // Obtenemos server y PORT mediante destructuring
 // server y PORT vienen del archivo index de la carpeta server
 // Nota: no es necesario agregar index al require de server
