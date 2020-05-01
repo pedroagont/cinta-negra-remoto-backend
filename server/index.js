f7683a89a3df0758529fd88d8040fe661895ee81
Pedro Gonzalez <pgonzalez@post.com>
2020-04-30 19:58:43 -0500

    Agregar middlewares

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const server = express();
 const PORT = process.env.PORT || 3000;
 
+//Middlewares
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
 //Endpoints
 server.get('/', (req, res) => res.send('Hello World!'));
 
@@ -13,4 +17,24 @@ server.get('/api/v1/temario/:id', (req, res) => {
   res.status(200).send('cool');
 });
 
-module.exports = { server, PORT }
+
+const Users = require('../models/Users');
+
+// CRUD Usuario
+
+// CREATE
+server.post('/api/v1/users', (req, res) => {
+  Users.create(req.body)
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send({ message: 'Error creating user', err }));
+});
+
+// GET (ALL)
+server.get('/api/v1/users', (req, res) => {
+  Users.find()
+    .then(users => res.status(200).send(users))
+    .catch(err => res.status(404).send({ message: 'Users not found', err }));
+});
+
+// exportar server para poder requerirlo desde otros archivos
+module.exports = { server, PORT };
