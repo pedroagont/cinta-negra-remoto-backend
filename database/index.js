274877ab67aca90ed46da9704f77e57dbc02a2ad
Pedro Gonzalez <pgonzalez@post.com>
2020-04-30 19:58:15 -0500

    Quitar respuesta de BD en consola

--- database/index.js
+++ database/index.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(res => console.log('Conectado a la BD correctamente', res))
+  .then(res => console.log('Conectado a la BD correctamente'))
   .catch(err => console.log('Error al conectarme...', err));
