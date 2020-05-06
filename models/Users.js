50dde455c74198a59f1a924a33d27f4b9414f093
Pedro Gonzalez <pgonzalez@post.com>
2020-05-06 01:42:58 -0500

    Instalar bcrypt y agregarlo como middleware para hashear contraseñas

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
 
 const usersSchema = new Schema({
   first_name: {
@@ -18,7 +20,40 @@ const usersSchema = new Schema({
     type: Boolean,
     default: true,
   },
+  password: {
+    type: String,
+    required: true,
+  }
 });
+
+//Referencia: https://www.mongodb.com/blog/post/password-authentication-with-mongoose-part-1
+usersSchema.pre('save', function(next) {
+  const user = this;
+
+  // only hash the password if it has been modified (or is new)
+  if (!user.isModified('password')) return next();
+
+  // generate a salt
+  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    // hash the password using our new salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+
+      // override the cleartext password with the hashed one
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+usersSchema.methods.comparePassword = function(candidatePassword) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return false;
+    return isMatch;
+  });
+};
 
 const Users = mongoose.model('User', usersSchema);
 
