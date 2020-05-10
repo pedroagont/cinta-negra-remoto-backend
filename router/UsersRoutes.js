fe77792b7b16434c2c55e12c4dd5da5b446fddc3
Pedro Gonzalez <pgonzalez@post.com>
2020-05-10 02:45:59 -0500

    Agregar verificación de token y prueba en get

--- router/UsersRoutes.js
+++ router/UsersRoutes.js
@@ -2,13 +2,28 @@ const express = require('express');
 const router = express.Router();
 const { UsersController } = require('../controller');
 
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  try {
+    const { authorization } = req.headers;
+    const token = authorization.split(' ')[1];
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.decoded = decoded;
+    next();
+  } catch (err) {
+    res.status(403).send({ message: ' Error with token', err })
+  }
+}
+
+
 // CRUD Usuario
 
 // CREATE
 router.post('/users', UsersController.create);
 
 // GET (ALL)
-router.get('/users', UsersController.find);
+router.get('/users', verifyToken, UsersController.find);
 
 // GET (ONE)
 router.get('/users/:id', UsersController.findById);
