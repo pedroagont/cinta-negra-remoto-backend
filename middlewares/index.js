df13c42c827ce0776d879df3c1f4d03813d3d4b5
Pedro Gonzalez <pgonzalez@post.com>
2020-05-13 22:03:13 -0500

    Agregar comentario sobre Bearer token y validación de authorization

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,7 +3,9 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   verifyToken: (req, res, next) => {
     try {
+      if (!req.headers.authorization) res.status(403).send({ error: "Authorization header must be provided"})
       const { authorization } = req.headers;
+      //authorization contiene: "Bearer token"
       const token = authorization.split(' ')[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.decoded = decoded;
